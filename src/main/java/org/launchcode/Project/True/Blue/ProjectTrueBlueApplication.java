0795987c44451336467cbd6ae465012a0dee1c4f
Your Name <apierpon@gmail.com>
2019-11-25 11:33:55 -0500

    initial commit of all files

---
+++ src/main/java/org/launchcode/Project/True/Blue/ProjectTrueBlueApplication.java
@@ -0,0 +1,13 @@
+package org.launchcode.Project.True.Blue;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectTrueBlueApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectTrueBlueApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/launchcode/Project/True/Blue/ProjectTrueBlueApplicationTests.java
@@ -0,0 +1,13 @@
+package org.launchcode.Project.True.Blue;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectTrueBlueApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
